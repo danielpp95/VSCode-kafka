731e8d4a50f875e81a38c256c380a8fa192399a6
Daniel Pereira <danielpp95@outlook.com>
2020-05-23 14:53:17 +0100

    create tree view

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,9 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  vscode.window.registerTreeDataProvider('exampleView', new TreeDataProvider());
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "q" is now active!');
+  console.log('Congratulations, your extension "q" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -20,8 +15,42 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage('Hello World from !');
 	});
 
-	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+class TreeDataProvider implements vscode.TreeDataProvider<TreeItem> {
+  onDidChangeTreeData?: vscode.Event<TreeItem|null|undefined>|undefined;
+
+  data: TreeItem[];
+
+  constructor() {
+    this.data = [new TreeItem('cars', [
+      new TreeItem(
+          'Ford', [new TreeItem('Fiesta'), new TreeItem('Focus'), new TreeItem('Mustang')]),
+      new TreeItem(
+          'BMW', [new TreeItem('320'), new TreeItem('X3'), new TreeItem('X5')])
+    ])];
+  }
+
+  getTreeItem(element: TreeItem): vscode.TreeItem|Thenable<vscode.TreeItem> {
+    return element;
+  }
+
+  getChildren(element?: TreeItem|undefined): vscode.ProviderResult<TreeItem[]> {
+    if (element === undefined) {
+      return this.data;
+    }
+    return element.children;
+  }
+}
+
+class TreeItem extends vscode.TreeItem {
+  children: TreeItem[]|undefined;
+
+  constructor(label: string, children?: TreeItem[]) {
+    super(
+        label,
+        children === undefined ? vscode.TreeItemCollapsibleState.None :
+                                 vscode.TreeItemCollapsibleState.Expanded);
+    this.children = children;
+  }
+}
